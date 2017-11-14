28ae261ae1c273ce153c67dc68d98ac2872be5b7
Kelly Stoops <kstoops815@gmail.com>
2017-11-13 21:36:57 -0600

    authentication

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,5 +1,14 @@
 "use strict";
 
-app.controller("AuthCtrl", function($scope){
-	$scope.controller = "AuthCtrl";
+app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthService){
+	$scope.authenticate = () => {
+		AuthService.authenticateGoogle().then((result) => {
+			$rootScope.uid = result.user.uid;
+			$scope.$apply(() => {
+				$location.url("/search");
+			});			
+		}).catch((error) => {
+			console.log("error in authenticateGoogle", error);
+		});
+	};
 });
