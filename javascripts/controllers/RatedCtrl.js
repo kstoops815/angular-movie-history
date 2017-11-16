dea4133a1380a0aed7a186876dd0f9103f3e0c3c
kstoops815 <kstoops815@gmail.com>
2017-11-15 21:40:09 -0600

    Revert "delete movies from wishlist and rated pages"

--- javascripts/controllers/RatedCtrl.js
+++ javascripts/controllers/RatedCtrl.js
@@ -3,22 +3,9 @@
 app.controller("RatedCtrl", function($rootScope, $scope, MovieService){
 	$scope.movies = [];
 
-	const getMovies = () => {
-		MovieService.getRatedMovies($rootScope.uid).then((results) =>{
+	MovieService.getRatedMovies($rootScope.uid).then((results) =>{
 		$scope.movies = results;
 	}).catch((error) => {
 		console.log("error in getRatedMovies", error);
 	});
-	};
-
-	getMovies();
-
-
-	$scope.deleteMovie = (movieId) => {
-		MovieService.deleteMovie(movieId).then((result) =>{
-			getMovies();
-		}).catch((error) => {
-			console.log("error in deleteMovie", error);
-		});
-	};
 });
--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -1,25 +1,9 @@
 "use strict";
 
 app.controller("WishlistCtrl", function($rootScope, $scope, MovieService){
-
-	const getMovies = () => {
-		MovieService.getWishlistMovies($rootScope.uid).then((results) =>{
+	MovieService.getWishlistMovies($rootScope.uid).then((results) =>{
 		$scope.movies = results;
-		}).catch((error) => {
+	}).catch((error) => {
 		console.log("error in getRatedMovies", error);
-		});
-	};
-
-	getMovies();
-	
-
-
-	$scope.deleteMovie = (movieId) => {
-		MovieService.deleteMovie(movieId).then((result) =>{
-			getMovies();
-		}).catch((error) => {
-			console.log("error in deleteMovie", error);
-		});
-	};
-
+	});
 });
