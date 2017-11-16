263ea026c4efe1807c7c98318be96a2e03ea5b77
Kelly Stoops <kstoops815@gmail.com>
2017-11-15 20:57:09 -0600

    can add rated movie

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,7 +1,18 @@
 "use strict";
 
-app.controller("SearchCtrl", function($scope, tmdbService){
+app.controller("SearchCtrl", function($location, $rootScope, $scope, MovieService, tmdbService){
 	$scope.movies = [];
+
+	const createMovie = (movie) => {
+		return {
+			"title": movie.title,
+			"overview": movie.overview,
+			"poster_path": movie.poster_path,
+			"rating": 0,
+			"isWatched": true,
+			"uid": $rootScope.uid
+		};
+	};
 
 	$scope.enterPush = (event) => {
 		if(event.keyCode === 13){
@@ -15,5 +26,19 @@ app.controller("SearchCtrl", function($scope, tmdbService){
 		
 	};
 
-	
+	$scope.saveRated = (tmdbMovie) => {
+		console.log("tmdbMovie", tmdbMovie);
+		let newMovie = createMovie(tmdbMovie);
+		MovieService.postNewMovie(newMovie).then(() => {
+			$location.path('/rated');
+		}).catch((error) => {
+			console.log("error in postNewMovie", error);
+		});
+	};
+
+
+
+
+
+
 });
