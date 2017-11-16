8a0f47df991edcef0f5756d39819d99c67c1e391
Kelly Stoops <kstoops815@gmail.com>
2017-11-15 21:31:42 -0600

    delete movies from wishlist and rated pages

--- javascripts/controllers/RatedCtrl.js
+++ javascripts/controllers/RatedCtrl.js
@@ -3,9 +3,22 @@
 app.controller("RatedCtrl", function($rootScope, $scope, MovieService){
 	$scope.movies = [];
 
-	MovieService.getRatedMovies($rootScope.uid).then((results) =>{
+	const getMovies = () => {
+		MovieService.getRatedMovies($rootScope.uid).then((results) =>{
 		$scope.movies = results;
 	}).catch((error) => {
 		console.log("error in getRatedMovies", error);
 	});
+	};
+
+	getMovies();
+
+
+	$scope.deleteMovie = (movieId) => {
+		MovieService.deleteMovie(movieId).then((result) =>{
+			getMovies();
+		}).catch((error) => {
+			console.log("error in deleteMovie", error);
+		});
+	};
 });
