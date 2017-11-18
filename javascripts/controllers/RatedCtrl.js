6f5f817e29029ea55928b2ff7a8de79f47b529f7
Kelly Stoops <kstoops815@gmail.com>
2017-11-18 10:54:30 -0600

    magnifying button added for rating page

--- javascripts/controllers/RatedCtrl.js
+++ javascripts/controllers/RatedCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller("RatedCtrl", function($rootScope, $scope, MovieService){
+app.controller("RatedCtrl", function($location, $rootScope, $scope, MovieService){
 	$scope.movies = [];
 
 	const getMovies = () => {
@@ -34,4 +34,9 @@ app.controller("RatedCtrl", function($rootScope, $scope, MovieService){
 			});
 		}
 	};
+
+	$scope.movieDetail = (movieId) => {
+		$location.path(`/movie/${movieId}`);
+	};
+
 });
