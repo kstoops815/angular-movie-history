dda58c4f1003b38b5da80004dd8ab6ac44d669c6
Kelly Stoops <kstoops815@gmail.com>
2017-11-18 10:32:00 -0600

    movie star rating working

--- javascripts/controllers/RatedCtrl.js
+++ javascripts/controllers/RatedCtrl.js
@@ -21,4 +21,17 @@ app.controller("RatedCtrl", function($rootScope, $scope, MovieService){
 			console.log("error in deleteMovie", error);
 		});
 	};
+
+
+	$scope.starChange = ($event, movie) => {
+		if(event.rating) {
+			movie.rating = event.rating;
+			let updatedMovie = MovieService.createMovieObject(movie);
+			MovieService.updateMovie(updatedMovie, movie.id).then(() => {
+				getMovies();
+			}).catch((error) => {
+				console.log("error in updateMovie", error);
+			});
+		}
+	};
 });
